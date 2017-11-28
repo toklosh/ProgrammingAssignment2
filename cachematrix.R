7d6ec5619daabca506b436621767f32e484ac4cd
neilo <admin@soccer-betbrain.com>
2017-11-28 20:39:05 +0200

    messing about

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
 ## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+ 
+##...function sets up inverse matrix environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invmatrix<- NULL
+  set <- function(y) {
+    x <<- y
+    invmatrix<<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) invmatrix<<- inverse
+  getinverse <- function() invmatrix
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invmatrix<- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get()
+  invmatrix<- solve(data)
+  x$setinverse(inv)
+  inv
 }
